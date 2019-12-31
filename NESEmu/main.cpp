5fa98afe9ef4443f276c8957b6ad2e1c0e4e6d45
Jonathan Baliko <johnmph@macbook-pro-de-jonathan.local>
2019-12-31 02:59:10 +0100

    Initial Commit

---
+++ NESEmu/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NESEmu
+//
+//  Created by Jonathan Baliko on 31/12/19.
+//  Copyright © 2019 Jonathan Baliko. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
